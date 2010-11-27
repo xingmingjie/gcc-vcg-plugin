9ede12a2dee53b2113c9ab090b6074716b2fe62e
mingjie.xing <mingjie.xing@1cc14b40-4d1a-70b5-55de-b264b92270c1>
2010-11-27 06:05:34 +0000

    update

--- trunk/vcg-plugin/vcg-plugin.h
+++ trunk/vcg-plugin/vcg-plugin.h
@@ -4,7 +4,7 @@
 
    This program is free software: you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
-   the Free Software Foundation, either version 2 of the License, or
+   the Free Software Foundation, either version 3 of the License, or
    (at your option) any later version.
 
    This program is distributed in the hope that it will be useful,
@@ -30,6 +30,9 @@
 #include "intl.h"
 #include "langhooks.h"
 #include "cfghooks.h"
+
+/* libgdl */
+#include "gdl.h"
 
 /* dump file names */
 
