f701c0c89750c3492147b0e3ff279e85265bd78d
mingjie.xing <mingjie.xing@1cc14b40-4d1a-70b5-55de-b264b92270c1>
2011-06-02 08:02:33 +0000

    support -fplugin-arg-vcg_plugin-cgraph-caller

--- vcg-plugin/gcc-cgraph.c
+++ vcg-plugin/gcc-cgraph.c
@@ -18,6 +18,8 @@
 #include "vcg-plugin.h"
 
 static htab_t title_table;
+
+/* Should be enough.  */
 static char buf[1024];
 
 /* For the hash tables.  */
@@ -28,12 +30,15 @@ string_hash_eq (const void *y1, const void *y2)
   return strcmp ((const char *) y1, (const char *) y2) == 0;
 }
 
+/* Get the gdl node label based on cgraph_node.  */
 
 static char*
 get_label (struct cgraph_node *node)
 {
   return (char *) cgraph_node_name (node);
 }
+
+/* Get the gdl node title based on cgraph_node and PREFIX.  */
 
 static char*
 get_title (struct cgraph_node *node, char *prefix)
@@ -57,6 +62,8 @@ get_title (struct cgraph_node *node, char *prefix)
   return title;
 }
 
+/* Create gdl edge based on EDGE and PREFIX.  */
+
 static void
 create_edge (gdl_graph *graph, struct cgraph_edge *edge, char *prefix)
 {
@@ -71,6 +78,8 @@ create_edge (gdl_graph *graph, struct cgraph_edge *edge, char *prefix)
   gdl_new_graph_edge (graph, source_title, target_title);
 }
 
+/* Create gdl node based on NODE and PREFIX.  */
+
 static void
 create_node (gdl_graph *graph, struct cgraph_node *node, char *prefix)
 {
@@ -83,10 +92,13 @@ create_node (gdl_graph *graph, struct cgraph_node *node, char *prefix)
   gdl_set_node_label (gnode, label);
 }
 
+/* Create specific call graph start from NODE.  Callee graph when CALLEE_P
+   is 1, caller graph when CALLEE_P is 0.  */
+
 static void
-create_node_and_edges_callee (gdl_graph *graph, struct cgraph_node *node)
+create_node_and_edges_specific (gdl_graph *graph, struct cgraph_node *node, int callee_p)
 {
-  struct cgraph_node *source, *target;
+  struct cgraph_node *source, *target, *node_x;
   struct cgraph_edge *edge;
   struct cgraph_edge **stack;
   char *prefix;
@@ -100,7 +112,11 @@ create_node_and_edges_callee (gdl_graph *graph, struct cgraph_node *node)
   stack = XNEWVEC (struct cgraph_edge *, cgraph_n_nodes + 1);
   sp = 0;
 
-  edge = node->callees;
+  if (callee_p)
+    edge = node->callees;
+  else
+    edge = node->callers;
+
   while (1)
     {
       while (edge)
@@ -108,37 +124,51 @@ create_node_and_edges_callee (gdl_graph *graph, struct cgraph_node *node)
           source = edge->caller;
           target = edge->callee;
 
+          if (callee_p)
+            node_x = target;
+          else
+            node_x = source;
+
           if (gdl_find_edge (graph, get_title (source, prefix),
                              get_title (target, prefix)) == NULL)
             create_edge (graph, edge, prefix);
 
-          if (gdl_find_node (graph, get_title (target, prefix)) == NULL)
+          if (gdl_find_node (graph, get_title (node_x, prefix)) == NULL)
             {
-              create_node (graph, target, prefix);
+              create_node (graph, node_x, prefix);
 
               stack[++sp] = edge;
-              edge = target->callees;
+              if (callee_p)
+                edge = node_x->callees;
+              else
+                edge = node_x->callers;
 
               continue;
             }
 
-          edge = edge->next_callee;
+          if (callee_p)
+            edge = edge->next_callee;
+          else
+            edge = edge->next_caller;
         }
 
       if (sp == 0)
         break;
 
       edge = stack[sp--];
-      edge = edge->next_callee;
+      if (callee_p)
+        edge = edge->next_callee;
+      else
+        edge = edge->next_caller;
     }
   free (stack);
   htab_delete (title_table);
 }
 
-/* Dump callee graph into the file FNAME.  */
+/* Dump specific call graph into the file FNAME.  */
 
 static void
-dump_cgraph_callee_to_file (char *fname)
+dump_cgraph_to_file_specific (char *fname, int callee_p)
 {
   gdl_graph *graph;
   struct cgraph_node *node;
@@ -147,9 +177,38 @@ dump_cgraph_callee_to_file (char *fname)
   gdl_set_graph_orientation (graph, "left_to_right");
 
   for (node = cgraph_nodes; node; node = node->next)
-    create_node_and_edges_callee (graph, node);
+    create_node_and_edges_specific (graph, node, callee_p);
 
   vcg_plugin_common.dump (fname);
+}
+
+/* Public function to dump caller graph.  */
+
+void
+vcg_plugin_dump_cgraph_caller (void)
+{
+  char *fname;
+
+  vcg_plugin_common.init ();
+
+  fname = concat (dump_base_name, ".cgraph-caller.vcg", NULL);
+  dump_cgraph_to_file_specific (fname, 0);
+  free (fname);
+
+  vcg_plugin_common.finish ();
+}
+
+/* Public function to view caller graph.  */
+
+void
+vcg_plugin_view_cgraph_caller (void)
+{
+  vcg_plugin_common.init ();
+
+  dump_cgraph_to_file_specific (vcg_plugin_common.temp_file_name, 0);
+  vcg_plugin_common.show (vcg_plugin_common.temp_file_name);
+
+  vcg_plugin_common.finish ();
 }
 
 /* Public function to dump callee graph.  */
@@ -162,7 +221,7 @@ vcg_plugin_dump_cgraph_callee (void)
   vcg_plugin_common.init ();
 
   fname = concat (dump_base_name, ".cgraph-callee.vcg", NULL);
-  dump_cgraph_callee_to_file (fname);
+  dump_cgraph_to_file_specific (fname, 1);
   free (fname);
 
   vcg_plugin_common.finish ();
@@ -175,7 +234,7 @@ vcg_plugin_view_cgraph_callee (void)
 {
   vcg_plugin_common.init ();
 
-  dump_cgraph_callee_to_file (vcg_plugin_common.temp_file_name);
+  dump_cgraph_to_file_specific (vcg_plugin_common.temp_file_name, 1);
   vcg_plugin_common.show (vcg_plugin_common.temp_file_name);
 
   vcg_plugin_common.finish ();
