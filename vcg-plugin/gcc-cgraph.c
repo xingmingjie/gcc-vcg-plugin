b6e58614e88980fa69c0d5c93b6c4addd4e95b1a
mingjie.xing <mingjie.xing@1cc14b40-4d1a-70b5-55de-b264b92270c1>
2011-06-02 02:48:30 +0000

    update

--- vcg-plugin/gcc-cgraph.c
+++ vcg-plugin/gcc-cgraph.c
@@ -17,31 +17,122 @@
 
 #include "vcg-plugin.h"
 
+static htab_t title_table;
+static char buf[1024];
+
+/* For the hash tables.  */
+
+static int
+string_hash_eq (const void *y1, const void *y2)
+{
+  return strcmp ((const char *) y1, (const char *) y2) == 0;
+}
+
+
+static char*
+get_label (struct cgraph_node *node)
+{
+  return (char *) cgraph_node_name (node);
+}
+
+static char*
+get_title (struct cgraph_node *node, char *prefix)
+{
+  char **slot;
+  char *title;
+
+  if (prefix == NULL)
+    return get_label (node);
+
+  sprintf (buf, "%s.%s", prefix, (char *) cgraph_node_name (node));
+  title = htab_find (title_table, buf);
+  if (title == NULL)
+    {
+      vcg_plugin_common.buf_print ("%s", buf);
+      title = vcg_plugin_common.buf_finish ();
+      slot = (char **) htab_find_slot (title_table, buf, INSERT);
+      *slot = title;
+    }
+
+  return title;
+}
+
+static void
+create_edge (gdl_graph *graph, struct cgraph_edge *edge, char *prefix)
+{
+  char *source_title, *target_title;
+  struct cgraph_node *source, *target;
+  
+  source = edge->caller;
+  target = edge->callee;
+
+  source_title = get_title (source, prefix);
+  target_title = get_title (target, prefix);
+  gdl_new_graph_edge (graph, source_title, target_title);
+}
+
+static void
+create_node (gdl_graph *graph, struct cgraph_node *node, char *prefix)
+{
+  char *title, *label;
+  gdl_node *gnode;
+
+  title = get_title (node, prefix);
+  label = get_label (node);
+  gnode = gdl_new_graph_node (graph, title);
+  gdl_set_node_label (gnode, label);
+}
+
 static void
 create_node_and_edges_callee (gdl_graph *graph, struct cgraph_node *node)
 {
+  struct cgraph_node *source, *target;
   struct cgraph_edge *edge;
-  gdl_node *src, *dest;
-  char *title_src, *title_dest;
-  char *label;
+  struct cgraph_edge **stack;
+  char *prefix;
+  int sp;
 
-  label = (char *) cgraph_node_name (node);
-  title_src = label;
-  src = gdl_new_graph_node (graph, title_src);
-  gdl_set_node_label (src, label);
+  title_table = htab_create (10, htab_hash_string, string_hash_eq, NULL);
 
-  for (edge = node->callees; edge; edge = edge->next_callee)
+  prefix = get_label (node);
+  create_node (graph, node, prefix);
+
+  stack = XNEWVEC (struct cgraph_edge *, cgraph_n_nodes + 1);
+  sp = 0;
+
+  edge = node->callees;
+  while (1)
     {
-      label = (char *) cgraph_node_name (edge->callee);
-      vcg_plugin_common.buf_print ("%s.%s", title_src, label);
-      title_dest = vcg_plugin_common.buf_finish ();
-      if (gdl_find_edge (graph, title_src, title_dest) == NULL)
+      while (edge)
         {
-          dest = gdl_new_graph_node (graph, title_dest);
-          gdl_set_node_label (dest, label);
-          gdl_new_graph_edge (graph, title_src, title_dest);
+          source = edge->caller;
+          target = edge->callee;
+
+          if (gdl_find_edge (graph, get_title (source, prefix),
+                             get_title (target, prefix)) == NULL)
+            create_edge (graph, edge, prefix);
+
+          if (gdl_find_node (graph, get_title (target, prefix)) == NULL)
+            {
+              create_node (graph, target, prefix);
+
+              stack[++sp] = edge;
+              edge = target->callees;
+
+              continue;
+            }
+
+          edge = edge->next_callee;
         }
+
+      if (sp == 0)
+        break;
+
+      edge = stack[sp--];
+      edge = edge->next_callee;
     }
+  free (stack);
+  htab_delete (title_table);
 }
 
 /* Dump callee graph into the file FNAME.  */
@@ -66,9 +157,13 @@ dump_cgraph_callee_to_file (char *fname)
 void
 vcg_plugin_dump_cgraph_callee (void)
 {
+  char *fname;
+
   vcg_plugin_common.init ();
 
-  dump_cgraph_callee_to_file ("dump-cgraph-callee.vcg");
+  fname = concat (dump_base_name, ".cgraph-callee.vcg", NULL);
+  dump_cgraph_callee_to_file (fname);
+  free (fname);
 
   vcg_plugin_common.finish ();
 }
@@ -126,9 +221,13 @@ dump_cgraph_to_file (char *fname)
 void
 vcg_plugin_dump_cgraph (void)
 {
+  char *fname;
+
   vcg_plugin_common.init ();
 
-  dump_cgraph_to_file ("dump-cgraph.vcg");
+  fname = concat (dump_base_name, ".cgraph.vcg", NULL);
+  dump_cgraph_to_file (fname);
+  free (fname);
 
   vcg_plugin_common.finish ();
 }
